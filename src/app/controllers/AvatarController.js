6949789e69b54d480f3795794ec163501f9aa33b
GuiFerrari <guiferrari10@hotmail.com>
2020-04-02 01:04:04 -0300

    Created methods to upload and update avatar

---
+++ src/app/controllers/AvatarController.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+
+import Courier from '../models/Courier';
+import File from '../models/File';
+
+class AvatarController {
+  async update(req, res) {
+    const { originalname, filename: path } = req.file;
+
+    const courier = await Courier.findOne({
+      where: {
+        id: req.userId,
+      },
+    });
+
+    const file = await File.findOne({
+      where: {
+        id: courier.avatar_id,
+      },
+    });
+
+    const newFile = await File.create({
+      name: originalname,
+      path,
+    });
+
+    const { id, name, email } = await courier.update({
+      avatar_id: newFile.id,
+    });
+
+    fs.unlink(`tmp/uploads/${file.path}`, (err) => {
+      if (err) throw err;
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar: {
+        id: newFile.id,
+        name: newFile.name,
+        path: newFile.path,
+      },
+    });
+  }
+}
+
+export default new AvatarController();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,37 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import CourierController from './app/controllers/CourierController';
+import AvatarController from './app/controllers/AvatarController';
 
 import authMiddleware from './app/middlewares/auth';
+import adminMiddleware from './app/middlewares/admin';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('/recipient', RecipientController.store);
-routes.put('/recipient/:id', RecipientController.update);
+routes.post('/recipient', adminMiddleware, RecipientController.store);
+routes.put('/recipient/:id', adminMiddleware, RecipientController.update);
+
+routes.post(
+  '/couriers',
+  adminMiddleware,
+  upload.single('file'),
+  CourierController.store
+);
+routes.put('/couriers/:id', adminMiddleware, CourierController.update);
+
+routes.put(
+  '/couriers/avatar/:id',
+  upload.single('file'),
+  AvatarController.update
+);
 
 export default routes;
